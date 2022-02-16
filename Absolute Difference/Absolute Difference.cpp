29e95847147641d6894883b2019180dcb7a6b3a8
Kallegan <karl.berglund@futuregames.nu>
2022-02-16 08:59:03 +0100

    completed abs, temp converter, even or odd. Make calculator for fun.

--- Absolute Difference/Absolute Difference.cpp
+++ Absolute Difference/Absolute Difference.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    int A;
+    int B;
+    int absolutDifferense;
+
+    cout << "Hello! Please enter two whole numbers, positive or negative.\n";
+    cin >> A;
+    cin >> B;
+
+    absolutDifferense = A - B;
+
+    if (absolutDifferense < 0) //checks if C is smaller than 0 so we can tell if its a negative number.
+    {
+        absolutDifferense *= -1; //converting C to positive.
+    }
+    cout << "The absolute difference between A and B is: " << absolutDifferense;
+
+    system("pause>0");
 }
--- Small assignments Karl B/Small assignments Karl B.cpp
+++ Small assignments Karl B/Small assignments Karl B.cpp
@@ -1,20 +1,35 @@
-// Small assignments Karl B.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	float num1, num2;
+	char operation;
+	cout << "CACATRON CALC";
+	cin >> num1 >> operation >> num2;
+
+	switch (operation)
+	{
+	case '-':cout << num1 << operation << num2 << "=" << num1 - num2; break;
+	case '+':cout << num1 << operation << num2 << "=" << num1 + num2; break;
+	case '/':cout << num1 << operation << num2 << "=" << num1 / num2; break;
+	case '*':cout << num1 << operation << num2 << "=" << num1 * num2; break;
+	case 'x':cout << num1 << operation << num2 << "=" << num1 * num2; break;
+	case '%':
+		bool isNum1int, isNum2Int;
+		isNum1int = ((int)num1 == num1);
+		isNum2Int = ((int)num2 == num2);
+
+		if (isNum1int && isNum2Int)
+			cout << num1 << operation << num2 << "%" << (int)num1 % (int)num2;
+		else
+			cout << "Unvalid operation!";
+		break;
+
+		default:cout << "Please enter a valid operation!" << endl;
+
+		system("pause>0");
+	}
+
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
