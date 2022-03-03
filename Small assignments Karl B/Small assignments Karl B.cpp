f6b15c45077be343c11d18c09e3f496ff1f954c1
Kallegan <karl.berglund@futuregames.nu>
2022-03-03 16:17:26 +0100

    addad min max sum, inheritence, char occurence

--- Small assignments Karl B/Small assignments Karl B.cpp
+++ Small assignments Karl B/Small assignments Karl B.cpp
@@ -1,46 +1,51 @@
 #include <iostream>
+#include <string.h> 
 
 using namespace std;
 
-
-string palindrome(string userInput)
-{
-	bool flagPalindrome = true;
-	unsigned int sizeOfInput = userInput.length();
-
-	for (size_t i = 0; i < userInput.length()&& flagPalindrome; i++)
-	{
-		sizeOfInput--;
-		if (userInput[i] == userInput[sizeOfInput])
-		{
-			flagPalindrome = true;
-
-		}
-		else
-		{
-			flagPalindrome = false;
-			break;
-		}
-	}
-	if (flagPalindrome)
-	{
-		return "the word " + userInput + " is a palindrome!";
-	}
-	if (!flagPalindrome)
-	{
-		return "the word " + userInput + " isn't a palindrome!";
-	}
-}
+void print_resault(char* s);
 
 int main()
 {
-	string userInput;
-	cout << "Please enter a palindrome!" << endl;
-	cin >> userInput;
 
-	cout << palindrome(userInput);
+
+
+	char s[] = "abcdefghiabkasldasdcxz";
+	cout << "enter stuff";
+	cin >> s;
+
+	print_resault(s);
 
 	return 0;
-
 }
 
+void print_resault(char* s)
+{
+	int length = strlen(s);
+	//wanted to use length but moved on since I had to use a more complex method.
+	char unknown[26]; //todo try static cast length.
+	int counted = 0;
+
+	for (int i = 0; i < length; i++)
+	{
+		bool already_counted = false;
+		for (int j = 0; j < counted; j++)
+			if (s[i] == unknown[j])
+			{
+				already_counted = true;
+			}
+
+		if (already_counted) continue;
+
+		int count = 0;
+		for (int j = 0; j < length; j++)
+		{
+			if (s[i] == s[j]) count++;
+		}
+		printf("%c - %d\n", s[i], count);
+
+		unknown[counted] = s[i];
+		counted++;
+	}
+
+}
